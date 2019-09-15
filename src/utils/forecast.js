05c27800bab0000a750bd636f954d2d9b8cff2e4
Sushant Ratnam <sushant.ratnam@gmail.com>
2019-09-15 15:37:51 +0530

    Added high and low temp and about page

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -12,7 +12,8 @@ const forecast = (longitutde, latitude, callback) => {
         } else if (body.error) {
             callback(body.error, undefined)
         } else {
-            callback(undefined, `${body.daily.data[0].summary} It is currently ${body.currently.temperature} degrees. There is ${body.currently.precipProbability}% chance of rain`)
+            callback(undefined, `${body.daily.data[0].summary} It is currently ${body.currently.temperature} degrees. There is ${body.currently.precipProbability}% chance of rain. 
+            Today's high is ${body.daily.data[0].temperatureHigh} and low is ${body.daily.data[0].temperatureLow}`)
 
         }
     })
